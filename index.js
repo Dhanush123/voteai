ed835d66f97edf0cc05d0d12ad1b9a69a255d5f9
Dhanush Patel <dhanush.patel@ymail.com>
2018-03-17 14:37:34 -0700

    init
    pt 1

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const server = express();
+server.use(bodyParser.json());
+
+server.post('/', function (req, res) {
+  console.log('webhook request');
+  try {
+      if (req.body) {
+          var requestBody = req.body;
+          if (requestBody.result) {
+            if (requestBody.result.action == 'photo_auth') {
+              console.log("inside photo_auth intent");
+              res.speech("Your body is:",JSON.stringify(requestBody));
+            }
+          }
+      }
+  }
+  catch (err) {
+    console.error('Cannot process request', err);
+    return res.status(400).json({
+        status: {
+            code: 400,
+            errorType: err.message
+        }
+    });
+  }
+});
+
+server.listen((process.env.PORT || 8000), function () {
+  console.log('Server listening');
+});
